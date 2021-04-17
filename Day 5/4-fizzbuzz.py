15d3be54e851f25aaab3e5ab4d4ccced1636dbef
Amsal Khan <amsalkhan@iul.ac.in>
2021-04-17 12:23:03 +0530

    fizzbuzz

---
+++ Day 5/4-fizzbuzz.py
@@ -0,0 +1,12 @@
+for num in range(1,100):
+    if(num%3==0 and num%5==0):
+        print("FizzBuzz")
+        continue
+    elif(num%3==0):
+        print("Fizz")
+        continue
+    elif(num%5==0):
+        print("Buzz")
+        continue
+    else:
+        print(num)
